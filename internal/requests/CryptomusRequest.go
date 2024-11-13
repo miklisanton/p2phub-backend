17d9d0d1deb904dd39cb16e08c6b013743ba3891
miklisanton <miklis2003@mail.ru>
2024-11-13 13:49:52 +0300

    fix: added omitempty

--- internal/requests/CryptomusRequest.go
+++ internal/requests/CryptomusRequest.go
@@ -11,30 +11,30 @@ type CryptomusRequest struct {
 
 // ConfirmRequest is a webhook request from cryptomus
 type ConfirmRequest struct {
-	Type             *string      `json:"type"`
-	UUID             *string      `json:"uuid"`
-	OrderID          *string      `json:"order_id"`
-	Amount           *string      `json:"amount"`
-	PaymentAmount    *string      `json:"payment_amount"`
-	PaymentAmountUSD *string      `json:"payment_amount_usd"`
-	MerchantAmount   *string      `json:"merchant_amount"`
-	Commission       *string      `json:"commission"`
-	IsFinal          *bool        `json:"is_final"`
-	Status           *string      `json:"status"`
-	From             *string      `json:"from"`
-	WalletAddressUUID *string     `json:"wallet_address_uuid"`
-	Network          *string      `json:"network"`
-	Currency         *string      `json:"currency"`
-	PayerCurrency    *string      `json:"payer_currency"`
-	AdditionalData   *string      `json:"additional_data"`
-	Txid             *string      `json:"txid"`
-	Convert          *ConvertData `json:"convert"`
-	Signature        *string      `json:"sign"`
+	Type              *string     `json:"type,omitempty"`
+	UUID              *string     `json:"uuid,omitempty"`
+	OrderID           *string     `json:"order_id,omitempty"`
+	Amount            *string     `json:"amount,omitempty"`
+	PaymentAmount     *string     `json:"payment_amount,omitempty"`
+	PaymentAmountUSD  *string     `json:"payment_amount_usd,omitempty"`
+	MerchantAmount    *string     `json:"merchant_amount,omitempty"`
+	Commission        *string     `json:"commission,omitempty"`
+	IsFinal           *bool       `json:"is_final,omitempty"`
+	Status            *string     `json:"status,omitempty"`
+	From              *string     `json:"from,omitempty"`
+	WalletAddressUUID *string     `json:"wallet_address_uuid,omitempty"`
+	Network           *string     `json:"network,omitempty"`
+	Currency          *string     `json:"currency,omitempty"`
+	PayerCurrency     *string     `json:"payer_currency,omitempty"`
+	AdditionalData    *string     `json:"additional_data,omitempty"`
+	Txid              *string     `json:"txid,omitempty"`
+	Convert           *ConvertData `json:"convert,omitempty"`
+	Signature         *string     `json:"sign,omitempty"`
 }
 
 type ConvertData struct {
-	ToCurrency *string `json:"to_currency"`
-	Commission *string `json:"commission"`
-	Rate       *string `json:"rate"`
-	Amount     *string `json:"amount"`
+	ToCurrency *string `json:"to_currency,omitempty"`
+	Commission *string `json:"commission,omitempty"`
+	Rate       *string `json:"rate,omitempty"`
+	Amount     *string `json:"amount,omitempty"`
 }
